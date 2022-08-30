ee2b2bf01a0584379c16310e216b9af708e47bbd
Calvin <calvin.lieu@gmail.com>
2022-08-30 16:35:42 -0700

    models and seeders for tweets and comments finished

---
+++ app/models/comment.py
@@ -0,0 +1,23 @@
+from .db import db
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(280), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    tweet_id = db.Column(db.Integer, db.ForeignKey('tweets.id'), nullable=False)
+    likes = db.Column(db.Integer, default=0)
+
+    tweet = db.relationship("Tweet", back_populates="comments")
+    user = db.relationship("User", back_populates="comments")
+    like_list = db.relationship("Like", back_populates="comment", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user": self.user.to_dict(),
+            "tweet": self.tweet_id,
+            "likes": self.likes
+        }
