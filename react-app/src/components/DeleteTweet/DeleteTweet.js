a41b2c48cada000382ce962ac0080d3f381577d4
Calvin <calvin.lieu@gmail.com>
2022-09-09 11:08:08 -0700

    tweet error validation complete

--- react-app/src/components/DeleteTweet/DeleteTweet.js
+++ react-app/src/components/DeleteTweet/DeleteTweet.js
@@ -4,24 +4,32 @@ import { useHistory } from "react-router-dom";
 import { deleteTweetThunk } from "../../store/tweet";
 
 function DeleteTweet({ tweet, onClick }) {
-    let dispatch = useDispatch();
-    let history = useHistory();
+  let dispatch = useDispatch();
+  let history = useHistory();
 
-    const onDelete = () => {
-        dispatch(deleteTweetThunk(tweet.id))
-        history.push('/')
-    }
+  const onDelete = () => {
+    dispatch(deleteTweetThunk(tweet.id));
+    history.push("/");
+  };
 
-    return (
-        <div className="delete-post">
-            <div className="delete-head">
-                <h3>Delete post?</h3>
-                <div>Are you sure you want to delete this tweet?</div>
-            </div>
-            <div className="delete-option delete-button" onClick={onDelete}>Delete</div>
-            <div className="delete-option cancel" onClick={onClick}>Cancel</div>
+  return (
+    <div className="delete-post">
+      <div className="delete-head">
+        <h3 className="delete-top-modal">Delete post?</h3>
+        <div className="confirmation-delete-msg">
+          Are you sure you want to delete this tweet?
         </div>
-    )
+      </div>
+      <div className="delete-btns-outer">
+        <div className="delete-option cancel" onClick={onClick}>
+          Cancel
+        </div>
+        <div className="delete-option delete-button" onClick={onDelete}>
+          Delete
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default DeleteTweet
+export default DeleteTweet;
--- react-app/src/components/EditTweet/EditTweetForm.js
+++ react-app/src/components/EditTweet/EditTweetForm.js
@@ -1,71 +1,100 @@
-import React, { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router-dom'
-import { updateTweetThunk } from '../../store/tweet'
-import "./EditTweet.css"
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { updateTweetThunk } from "../../store/tweet";
+import "./EditTweet.css";
 
 function EditTweetForm({ tweet, onClick }) {
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const [ description, setDescription ] = useState(tweet?.description)
-    const [ imageUrl, setImageUrl ] = useState(tweet?.image_url)
-    const [errorValidators, setErrorValidators] = useState([]);
-    const user = useSelector(state => state.session.user)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [description, setDescription] = useState(tweet?.description);
+  const [imageUrl, setImageUrl] = useState(tweet?.image_url);
+  const [errors, setErrors] = useState([]);
+  const user = useSelector((state) => state.session.user);
 
+  useEffect(() => {
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
-        const payload = {
-            user_id: user.id,
-            description,
-            image_url: imageUrl
-        }
-
-        let updatedTweet = await dispatch(updateTweetThunk(payload, tweet.id))
-        if (updatedTweet) {
-            history.push(`/tweets/${tweet.id}`)
-        }
-
-        onClick()
+    const error = []
+    if (description.length < 0) {
+        error.push(["Tweet is required!"])
     }
-    return (
-        <div className="edit-post-container">
-            <div className="edit-post-top-bar">
-                <div onClick={() => onClick()}>Cancel</div>
-                <div style={{'font-weight': 'bold'}}>Edit Tweet</div>
-                <button onClick={handleSubmit} className='edit-post-submit-button' type='submit'>Done</button>
-            </div>
-            <div className="main-edit-container">
-                <div className="left-edit-container">
-                    <img className='edit-img' src={imageUrl} alt=""/>
-                </div>
+    setErrors(error)
+  }, [description])
 
-                <div className="right-edit-container">
-                    <form className='edit-post-form-container' onSubmit={handleSubmit}>
-                        <div className="user-info-modal">
-                            {tweet?.user?.profile_image_url ? (
-                                <img className='user-post-image' src={tweet.user.profile_image_url} alt="" />
-                            ) : (
-                                <img className='user-post-image' src="https://i.imgur.com/vF8FTS2.png" alt="Profile"/>
-                                )
-                            }
-                            <div className='user-post-username'>{tweet.user.username}</div>
-                        </div>
-                        <div className='post-caption-edit'>
-                            <textarea
-                                placeholder='Write a caption...'
-                                value={description}
-                                onChange={e => setDescription(e.target.value)}
-                                required
-                            />
-                        </div>
-                    </form>
-                </div>
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (errors.length > 0) {
+      return;
+    }
+
+    if (!description) {
+      setErrors(["Tweet is required!"]);
+      return;
+    }
+    if (description && description.trim().length === 0) {
+      setErrors(["Tweet is required!"]);
+      return;
+    }
+
+    if (description.length > 280) {
+        setErrors(["Maximum characters of 280 has been reached."])
+        return;
+    }
+
+    const payload = {
+      user_id: user.id,
+      description,
+      image_url: imageUrl,
+    };
+
+    let updatedTweet = await dispatch(updateTweetThunk(payload, tweet.id));
+    if (updatedTweet) {
+      history.push(`/tweets/${tweet.id}`);
+    }
+
+    onClick();
+  };
+  return (
+    <div className="edit-post-container">
+      <div className="edit-post-top-bar">
+        <div onClick={() => onClick()}>Cancel</div>
+        <div style={{ "font-weight": "bold" }}>Edit Tweet</div>
+        <button
+          onClick={handleSubmit}
+          className="edit-post-submit-button"
+          type="submit"
+        >
+          Edit
+        </button>
+      </div>
+      <div className="main-edit-container">
+        <div className="right-edit-container">
+          <form className="edit-post-form-container" onSubmit={handleSubmit}>
+            {errors && (
+              <>
+                {errors.map((error) => {
+                  return (
+                    <div className="edit-post-form-errors">{`${error}`}</div>
+                  );
+                })}
+              </>
+            )}
+            <div className="post-caption-edit">
+              <textarea
+                placeholder="Write a Tweet"
+                maxLength="281"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                required
+              />
             </div>
+          </form>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default EditTweetForm;
