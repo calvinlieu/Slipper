d59b16f3356f8aaffe5dca94790bd775aa87c8d6
Calvin <calvin.lieu@gmail.com>
2022-09-11 16:03:48 -0700

    added buttons to every button

--- react-app/src/components/CreateComment/index.js
+++ react-app/src/components/CreateComment/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import CommentForm from "./CreateCommentForm";
+import "./CreateComment.css"
 
 
 function CreateCommentModal({ tweet }) {
