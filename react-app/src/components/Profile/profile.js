d8a603cd2d1564689ed47807ab8cc1bd4afa693f
Calvin <calvin.lieu@gmail.com>
2022-10-05 14:42:31 -0700

    changes to profile and added profile pictures

--- react-app/src/components/Profile/profile.js
+++ react-app/src/components/Profile/profile.js
@@ -1,28 +1,30 @@
-import React, {useEffect } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import { useHistory, useParams } from 'react-router-dom';
-import { getProfileThunk } from '../../store/profile';
-
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory, useParams } from "react-router-dom";
+import { getProfileThunk } from "../../store/profile";
+import "./profile.css"
 
 const ProfilePage = () => {
-    const userSession = useSelector(state => state.session.user)
-    const userProfile = useSelector(state => state.profile);
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const { userId } = useParams();
+  const userSession = useSelector((state) => state?.session.user);
+  const userProfile = useSelector((state) => state?.profile);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const { userId } = useParams();
 
-    console.log(userProfile, "profile")
+  console.log(userProfile, "profile");
 
-    useEffect(() =>{
-        dispatch(getProfileThunk(userId))
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(getProfileThunk(userId));
+  }, [dispatch, userId]);
 
+  return (
+    <div>
+        <img className="profile-picture"src={userProfile.profile?.profile_image_url}></img>
+        <div>{userProfile.profile?.username}</div>
+        <div>@{userProfile.profile?.username}</div>
 
+    </div>
+  );
+};
 
-    return (
-        <div>hi</div>
-    )
-}
-
-
-export default ProfilePage
+export default ProfilePage;
